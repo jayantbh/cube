0f0e6d382d1ca5c44231d47653784d9774579b15
jayantbh <bhawal.jayant@gmail.com>
2015-03-29 02:11:15 +0530

    3D Cubes Initial Commit
    Just some 3D Cubes Spinning!

---
+++ index.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>3D Cube</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/less" href="css/style.less">
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/less.min.js"></script>
+</head>
+<body>
+	<div class="cubegrid-container">
+		<div class="cubegrid one">
+			<div class="container one">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container two">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container three">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container four">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container five">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container six">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container seven">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container eight">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container nine">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+		</div>
+		<div class="cubegrid two">
+			<div class="container one">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container two">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container three">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container four">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container five">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container six">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container seven">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container eight">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container nine">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+		</div>
+		<div class="cubegrid three">
+			<div class="container one">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container two">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container three">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container four">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container five">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container six">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container seven">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container eight">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container nine">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+		</div>
+		<div class="cubegrid four">
+			<div class="container one">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container two">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container three">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container four">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container five">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container six">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container seven">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container eight">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container nine">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+		</div>
+		<div class="cubegrid five">
+			<div class="container one">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container two">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container three">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container four">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container five">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container six">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container seven">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container eight">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container nine">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+		</div>
+		<div class="cubegrid six">
+			<div class="container one">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container two">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container three">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container four">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container five">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container six">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container seven">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container eight">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+			<div class="container nine">
+				<div class="face f1"></div>
+				<div class="face f2"></div>
+				<div class="face f3"></div>
+				<div class="face f4"></div>
+				<div class="face f5"></div>
+				<div class="face f6"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
